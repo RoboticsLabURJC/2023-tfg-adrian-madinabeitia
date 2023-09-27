f9de0b8a1977a8793e2d889aa88a7fd087d9d838
madinabeip <portanova2002@gmail.com>
2023-09-27 21:50:12 +0200

    Week 1 wiki

--- src/drone_driver/launch/ocean_simulation.launch.py
+++ src/drone_driver/launch/ocean_simulation.launch.py
@@ -1,13 +1,11 @@
-"""Simulate a Tello drone"""
-
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 
 ns = 'tello_drone'
+
 def generate_launch_description():
     world_path = os.path.join(get_package_share_directory('drone_driver'), 'worlds', 'ocean_simulation.world')
     urdf_path = os.path.join(get_package_share_directory('drone_driver'), 'urdf', 'tello.urdf')
@@ -15,16 +13,12 @@ def generate_launch_description():
     return LaunchDescription([
 
         # Spawn tello.urdf
-        Node(package='drone_driver', executable='inject_entity.py', output='screen',
-            arguments=[urdf_path, '0', '0', '2', '0']),
+        # Node(package='drone_driver', executable='inject_entity.py', output='screen',
+        #     arguments=[urdf_path, '0', '0', '2', '0']),
         
-        # Publish static transforms
-        Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen',
-             arguments=[urdf_path]),
-
-        # Joystick driver, generates /namespace/joy messages
-        Node(package='joy', executable='joy_node', output='screen',
-             namespace=ns),
+        # Set the robot_description parameter
+        # Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen', namespace=ns,
+        #      parameters=[{'robot_description': urdf_path}]),
 
         # Launch Gazebo, loading tello.world
         ExecuteProcess(cmd=[
@@ -33,5 +27,6 @@ def generate_launch_description():
             '-s', 'libgazebo_ros_init.so',  # Publish /clock
             '-s', 'libgazebo_ros_factory.so',  # Provide gazebo_ros::Node
             world_path
-        ], output='screen'),
+        ], 
+        output='screen'),
     ])
