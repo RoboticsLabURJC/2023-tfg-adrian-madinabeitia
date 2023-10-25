8834f8f0ebfaa1fce962c46f08424ce8a672e67b
madinabeip <portanova2002@gmail.com>
2023-10-25 12:27:59 +0200

    Close tmux sesion on exit

--- src/drone_driver/launch/as2_sim_circuit.launch.py
+++ src/drone_driver/launch/as2_sim_circuit.launch.py
@@ -1,7 +1,8 @@
 import os
+import subprocess
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -13,9 +14,13 @@ env_vars = {
     'AEROSTACK2_SIMULATION_DRONE_ID': namespace
 }
 
+def exit_process_function(_launch_context, route, ns):
+    subprocess.run(["bash", route, ns], check=True)
+
 def generate_launch_description():
     sim_config = os.path.join(get_package_share_directory('drone_driver'), 'config')
     worlds_dir = os.path.join(get_package_share_directory('drone_driver'), 'worlds')
+    utils_path = os.path.join(get_package_share_directory('drone_driver'), 'utils')
     tmux_yml = os.path.join(get_package_share_directory('drone_driver'), 'config/tmuxLaunch.yml')
 
     # Px4 autopilot gazebo launch
@@ -25,7 +30,16 @@ def generate_launch_description():
             f'$AS2_GZ_ASSETS_SCRIPT_PATH/default_run.sh {sim_config}/world.json'
         ],
         name="gazebo",
-        additional_env=env_vars
+        additional_env=env_vars,
+
+        # Closes the tmux sesion on gazebo exit
+        on_exit=[
+            OpaqueFunction(
+                function=exit_process_function,
+                args=[utils_path + '/end_tmux.sh', namespace]
+            ),
+            LogInfo(msg='Tmux session closed')
+            ]
     )
 
     # Prepares the tmux session
@@ -37,6 +51,7 @@ def generate_launch_description():
 
         name="tmuxLauncher",
         output='screen'
+
     )
 
     tmuxAttach = ExecuteProcess(
